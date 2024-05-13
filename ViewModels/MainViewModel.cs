7196896938bd0351b0ed6269118fa45d58982137
Plag45 <103328035+Plag45@users.noreply.github.com>
2024-05-13 17:42:59 +0300

    Add files via upload

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Aplicatie_de_Booking.Models;
+
+namespace Aplicatie_de_Booking.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Fields
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get
+            {
+                return _currentUserAccount;
+            }
+
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            // userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount.Username = user.Username;
+                CurrentUserAccount.DisplayName = $"Welcome {user.Name} {user.LastName} ;)";
+                CurrentUserAccount.ProfilePicture = null;
+            }
+            else
+            {
+                CurrentUserAccount.DisplayName = "Invalid user, not logged in";
+                //Hide child views.
+            }
+        }
+    }
+}
