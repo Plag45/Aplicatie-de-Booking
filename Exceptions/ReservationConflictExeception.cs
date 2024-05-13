ea9ed625dad0eeb025619bec05544358c98a2a6b
Plag45 <103328035+Plag45@users.noreply.github.com>
2024-05-13 17:46:30 +0300

    Add files via upload

---
+++ Exceptions/ReservationConflictExeception.cs
@@ -0,0 +1,28 @@
+﻿using Aplicatie_de_Booking.Models;
+
+namespace Aplicatie_de_Booking.Exceptions
+{
+    public class ReservationConflictException : Exception
+    {
+        public Reservation ExistingReservation { get; }
+        public Reservation IncomingReservation { get; }
+
+        public ReservationConflictException(Reservation existingReservation, Reservation incomingReservation)
+        {
+            ExistingReservation = existingReservation;
+            IncomingReservation = incomingReservation;
+        }
+
+        public ReservationConflictException(string message, Reservation existingReservation, Reservation incomingReservation) : base(message)
+        {
+            ExistingReservation = existingReservation;
+            IncomingReservation = incomingReservation;
+        }
+
+        public ReservationConflictException(string message, Exception innerException, Reservation existingReservation, Reservation incomingReservation) : base(message, innerException)
+        {
+            ExistingReservation = existingReservation;
+            IncomingReservation = incomingReservation;
+        }
+    }
+}
