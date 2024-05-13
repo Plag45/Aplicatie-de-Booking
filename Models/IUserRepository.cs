3ea4ea4ee7673172a2b658b37e98d47465770af3
Plag45 <103328035+Plag45@users.noreply.github.com>
2024-05-13 17:43:32 +0300

    Add files via upload

---
+++ Models/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Aplicatie_de_Booking.Models
+{
+    public interface IUserRepository
+    {
+        bool AuthenticateUser(NetworkCredential credential);
+        void Add(UserModel userModel);
+        void Edit(UserModel userModel);
+        void Remove(int id);
+        UserModel GetById(int id);
+        UserModel GetByUsername(string username);
+        IEnumerable<UserModel> GetByAll();
+        //...
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,31 @@
+﻿namespace Aplicatie_de_Booking.Models
+{
+    public class Reservation
+    {
+        public Room_ID Room_ID { get; }
+        public string Username { get; }
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+
+        public TimeSpan Lenght => EndTime.Subtract(StartTime);
+
+        public Reservation(Room_ID room_ID, DateTime startTime, DateTime endTime, string username)
+        {
+            Room_ID = room_ID;
+            StartTime = startTime;
+            EndTime = endTime;
+            Username = username;
+        }
+
+        public bool Conflicts(Reservation reservation)
+        {
+            if (reservation.Room_ID != Room_ID)
+            {
+                return false;
+
+            }
+
+            return reservation.StartTime < EndTime && reservation.EndTime > StartTime;
+        }
+    }
+}
